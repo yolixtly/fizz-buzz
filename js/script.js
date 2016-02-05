316950ba6c284f88769a5f1e3ed33c0f2864dfb4
yolixtly <yolixtly_13@hotmail.com>
2016-02-05 15:46:59 -0500

    first attempt to fizz

--- js/script.js
+++ js/script.js
@@ -1,52 +1,15 @@
 console.log("hello Yolix");
 
-var FizsBuzzOutput = '';
-    var fizz = 0;
-    var buzz = 0;
-    var fizzbuzz=0;
-
-
-
-    for ( var i=1; i < 100; i++ ) {
-
-
-        if (i % 15 == 0){
-            fizzbuzz+=1;                
-            FizsBuzzOutput += "FizsBuzz, ";
-        }
-
-        else if ( i % 3 == 0 ){
-            fizz+=1;
-            FizsBuzzOutput += "Fizz, ";
-        }
-
-
-        else if (i % 5 == 0){
-            buzz+=1;
-            FizsBuzzOutput += "Buzz, "
-        }       
-        else {
-
-            FizsBuzzOutput += i + ', ';
-        }
-        console.log(FizsBuzzOutput);
-    }
- console.log('fizz count= '+ fizz);
- console.log('buzz count= '+ buzz);
- console.log('fizzbuzz count= '+ fizzbuzz);
-
-
-
 // this one shows the fizz but stops counting: 
 
-// for(var i = 1; i < 11; i++) {
-// 	if(i % 10 === 0) {
-// 		var i = "fizz";
-// 	} else {
-// 		var i = i;
-// 	}
-// 	$("li").append(i).css("color", "hotpink");
-// }
+for(var i = 1; i < 11; i++) {
+	if(i % 10 === 0) {
+		var i = "fizz";
+	} else {
+		var i = i;
+	}
+	$("li").append(i).css("color", "hotpink");
+}
 
 
 
