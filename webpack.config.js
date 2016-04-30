9396b89279ca5c99e8229c2f7929449bef2f387c
yolixtly <yolixtly_13@hotmail.com>
2016-04-29 22:00:10 -0400

    Uglify error

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,15 @@ var webpack = require('webpack');
 
 var packageData = require('./package.json');
 
+var minify = process.argv.indexOf('--minify') != -1;
+
 var filename = [packageData.name, packageData.version, 'js'];
+var plugins = [];
+
+if (minify) {
+    filename.splice(filename.length - 1, 0, 'min');
+    plugins.push(new webpack.optimize.UglifyJsPlugin());
+}
 
 module.exports = {
     entry: path.resolve(__dirname, packageData.main),
@@ -16,13 +24,11 @@ module.exports = {
     module: {
       loaders: [
         {
-          test: /\.js$/,
+          test: /\.js?$/,
           exclude: /(node_modules)/,
-          loader: 'babel',
-          query: {
-            presets: ['es2015']
-          }
+          loader: 'babel'
         }
       ]
-    }
+    },
+    plugins: plugins
 }
